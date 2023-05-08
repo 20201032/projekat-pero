aa2c038532e49cda37305245aff8f8ceefe205f4
vlada05 <vladimirjov16@gmail.com>
2023-05-08 21:42:22 +0200

    sitna dorada

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 mongoose
   .connect(
-    "mongodb+srv://admin:cBY2pP2WZSsLmZnk@cluster0.jsjmsye.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://admin:cBY2pP2WZSsLmZnk@cluster0.kcumdgg.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => app.listen(5000))
   .then(() =>
